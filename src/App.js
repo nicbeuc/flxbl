25b0c9d837e852ab34238a80fc2fd9e0e81123f3
NicBeucM3 <nick.beuchat@gmail.com>
2022-11-08 10:12:16 -0500

    various updates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import { useState } from 'react';
 import './App.css';
+import ParamForm from './ParamForm.js';
+
+// Default Parameters
+const defaultParams = {
+  baseFontSize: 16,
+  children: 3,
+  gap: 1,
+  fillAvailable: true,
+}
+
+const defaultBreakpoints = {
+  mobile: [0, 499],
+  tablet: [500, 999],
+  desktop: [1000, 9999]
+}
 
 function App() {
+  const [params, setParams] = useState(defaultParams);
+  const [breakpoints, setBreakpoints] = useState(defaultBreakpoints);
+
   return (
     <div className="App">
       <h1>Flxbl</h1>
+      <ParamForm params={params}/>
     </div>
   );
 }
