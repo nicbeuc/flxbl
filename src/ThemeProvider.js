e3a26ff2cdc5d84e42ae7a1a95061c70be2118be
nicbeuc <nick.beuchat@gmail.com>
2022-12-10 19:31:13 -0500

    persistent dark mode

--- src/ThemeProvider.js
+++ src/ThemeProvider.js
@@ -1,9 +1,19 @@
-import { useState, createContext } from "react";
+import { useState, useEffect, createContext } from "react";
 
 const ThemeContext = createContext();
 
 function ThemeProvider({children}) {
-  const [darkMode, setDarkMode] = useState(true);
+  const [darkMode, setDarkMode] = useState(() => {
+    if (localStorage.getItem('darkMode') !== undefined) {
+      return localStorage.getItem('darkMode') === 'true';
+    } else {
+      return true;
+    }
+  });
+
+  useEffect(() => {
+    localStorage.setItem('darkMode', darkMode);
+  }, [darkMode]);
 
   function toggleTheme() {
     setDarkMode(!darkMode);
